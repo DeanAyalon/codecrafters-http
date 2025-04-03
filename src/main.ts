fb8183063633f4a642e7d5c28a8985a236df1b2e
Dean Ayalon <deanayalon@me.com>
2025-04-03 22:06:58 +0300

    Add HttpResponse class, 404 reponse Using built-in http library

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import net from 'net'
+import { HttpResponse } from './http.js'
 
 const server = net.createServer((socket) => 
     socket.on('close', () => socket.end())
@@ -7,11 +8,22 @@ const server = net.createServer((socket) =>
 server.listen(4221, '0.0.0.0')
 console.log('Listening on port 4221')
 
-server.on('connection', (socket) => {
-    console.log('Connection established', socket.remoteAddress)
-    socket.write('HTTP/1.1 200 OK\r\n')
-    socket.write('Content-Type: text/plain\r\n')
-    socket.write('\r\n') // Headers end
-    socket.write('Successfully returned 200!\r\n')
-    socket.end()
+server.on('connection', socket => {
+    function respond(code: number | string, message: string) {
+        console.log('->', socket.remoteAddress, code, message)
+        socket.write(new HttpResponse(code, message).toString())
+        socket.end()
+    }
+
+    // console.log('Connection established', socket.remoteAddress)
+    socket.on('data', data => {
+        // console.log('Received data:', data.toString())
+        const request = data.toString(),
+            reqWords = request.split(' '),
+            method = reqWords[0], url = reqWords[1],
+            path = url.split('/').filter(val => val.length > 0) // Sanitize path from empty sections
+        console.log(socket.remoteAddress, method, url)
+
+        respond(path.length ? 404 : 200, url)
+    })
 })
