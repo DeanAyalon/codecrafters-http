49716e346ff67c1307602049c005671391c1fd5b
Dean Ayalon <dev@deanayalon.com>
2025-04-09 14:05:43 +0300

    Read header - User-Agent

--- app/http/response.ts
+++ app/http/response.ts
@@ -1,10 +1,12 @@
+import { Socket } from 'net'
 import { STATUS_CODES } from 'http'
 import { Buffer } from 'buffer'
 
 const version = 1.1
 
-export class HttpResponse {
+export class Response {
     constructor(private code: number | string, private message?: string) {}
+    /** The response message */
     toString() {
         return [`HTTP/${version} ${this.code} ${STATUS_CODES[this.code]}`,
             'Content-Type: text/plain', 
@@ -13,5 +15,14 @@ export class HttpResponse {
             , this.message].join('\r\n')
     }
 
+    /** The Content-Length of the response message, in bytes */
     length() { return this.message ? Buffer.byteLength(this.message) : 0 }
+}
+
+export function  respond(socket: Socket, code: number | string, message?: string) {
+    console.log('->', socket.remoteAddress, code, message)
+    const response = new Response(code, message)
+    console.log('  ', response.length(), 'bytes')
+    socket.write(response.toString())
+    socket.end()
 }
--- app/main.ts
+++ app/main.ts
@@ -1,33 +1,27 @@
 import net from 'net'
-import { HttpResponse } from './http.js'
 
-const server = net.createServer((socket) =>
-    socket.on('close', () => socket.end())
-)
+import { Request } from './http/request.js'
+import { respond as _respond } from './http/response.js'
 
+const server = net.createServer((socket) => socket.on('close', () => socket.end()))
 server.listen(4221, '0.0.0.0')
 console.log('Listening on port 4221')
 
 server.on('connection', socket => {
-    function respond(code: number | string, message?: string) {
-        console.log('->', socket.remoteAddress, code, message)
-        const response = new HttpResponse(code, message)
-        console.log('  ', response.length(), 'bytes')
-        socket.write(response.toString())
-        socket.end()
-    }
-
     // console.log('Connection established', socket.remoteAddress)
     socket.on('data', data => {
         // console.log('Received data:', data.toString())
         const request = data.toString(),
-            reqWords = request.split(' '),
-            method = reqWords[0], url = reqWords[1],
-            path = url.split('/').filter(val => val.length > 0) // Sanitize path from empty sections
+            { method, url, path } = Request.title(request), // Parse first request line
+            respond = (code: number | string, message?: string) => _respond(socket, code, message)
+
         console.log(socket.remoteAddress, method, url)
 
-        if (!path.length) respond(200, 'Welcome!')
-        else if (path[0] == 'echo') respond(200, path[1])
-        else respond(404, url + ' not found')
+        switch(path[0]) {
+            case undefined: respond(200, 'Welcome!'); break
+            case 'echo': respond(200, path[1]); break
+            case 'user-agent': respond(200, Request.headers(request)['User-Agent']); break
+            default: respond(404, url + ' not found')
+        }
     })
 })
