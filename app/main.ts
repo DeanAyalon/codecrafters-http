53e2f314e5a130904c6a0a3c840aec60c9ac9665
Dean Ayalon <dev@deanayalon.com>
2025-04-09 13:54:26 +0300

    Return success

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,17 @@
-import net from "net"
+import net from 'net'
 
-const server = net.createServer(socket =>
-    socket.on("close", () => socket.end())
+const server = net.createServer((socket) => 
+    socket.on('close', () => socket.end())
 )
 
-server.listen(4221, "localhost")
+server.listen(4221, '0.0.0.0')
+console.log('Listening on port 4221')
+
+server.on('connection', (socket) => {
+    console.log('Connection established', socket.remoteAddress)
+    socket.write('HTTP/1.1 200 OK\r\n')
+    socket.write('Content-Type: text/plain\r\n')
+    socket.write('\r\n') // Headers end
+    socket.write('Successfully returned 200!\r\n')
+    socket.end()
+})
