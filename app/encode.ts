a4db13a418c50ba51ba9964eb729b9d9664b9f4f
Dean Ayalon <dev@deanayalon.com>
2025-04-09 15:19:12 +0300

    Support comma-separated 'Accept-Encoding' headers

--- app/encode.ts
+++ app/encode.ts
@@ -1,9 +1,13 @@
-export function selectEncoding(encoding: string | string[]) {
-    if (!encoding) return null
-    if (typeof encoding == 'string') encoding = [encoding]
+export function selectEncoding(acceptedEncodings: string | string[]) {
+    // Synthesize Accepted-Encoding headers (may be multi-lined, or comma separated)
+    if (!acceptedEncodings) return null
+    if (typeof acceptedEncodings == 'string') acceptedEncodings = [acceptedEncodings] // Single string - transform to array
+    const encodings: string[] = []
+    // Each header line may be comma-separated; Remove spaces, lowercase
+    acceptedEncodings.forEach(line => encodings.push(...(line.split(',').map(encoding => encoding.toLowerCase().replace(' ', '')))))
 
-    const encodings = encoding.map(e => e.toLowerCase())
+    // Supported encodings
     if (encodings.includes('gzip')) return 'gzip'
-    
+
     return null
 }
