7461482de9487b3575f3675d793465079c0389c9
Dean Ayalon <dev@deanayalon.com>
2025-04-09 14:41:48 +0300

    Respond with files Add --directory for files path

--- app/http/response.ts
+++ app/http/response.ts
@@ -2,27 +2,44 @@ import { Socket } from 'net'
 import { STATUS_CODES } from 'http'
 import { Buffer } from 'buffer'
 
-const version = 1.1
+const version = 1.1,
+    contentType = {
+        text: 'text/plain',
+        other: 'application/octet-stream'
+    }
 
 export class Response {
-    constructor(private code: number | string, private message?: string) {}
-    /** The response message */
-    toString() {
-        return [`HTTP/${version} ${this.code} ${STATUS_CODES[this.code]}`,
-            'Content-Type: text/plain', 
-            'Content-Length: ' + this.length(),
-            // Headers end
-            , this.message].join('\r\n')
+    private _contentType: string
+    get contentType() { return this._contentType }
+
+    constructor(private code: number | string, private payload?) {
+        if (typeof payload === 'string') this._contentType = contentType.text
+        else this._contentType = contentType.other
     }
 
     /** The Content-Length of the response message, in bytes */
-    length() { return this.message ? Buffer.byteLength(this.message) : 0 }
+    length() { return Buffer.byteLength(this.payload) }
+
+    headers() {
+        return [`HTTP/${version} ${this.code} ${STATUS_CODES[this.code]}`,
+            'Content-Type: ' + this._contentType,
+            'Content-Length: ' + this.length(),
+            '', // header-body separator
+            ''].join('\r\n')
+    }
+    payloadBuffer() { 
+        return Buffer.isBuffer(this.payload) ? this.payload : Buffer.from(this.payload)
+    }
 }
 
-export function  respond(socket: Socket, code: number | string, message?: string) {
-    console.log('->', socket.remoteAddress, code, message)
-    const response = new Response(code, message)
-    console.log('  ', response.length(), 'bytes')
-    socket.write(response.toString())
+export function  respond(socket: Socket, code: number | string, payload?: any) {
+    console.log('->', socket.remoteAddress, code, payload)
+    // console.log('->', socket.remoteAddress, code, (typeof payload === 'string' ? payload : '<binary>'))
+    const response = new Response(code, payload),
+        headers = response.headers(), body = response.payloadBuffer()
+    console.log('  ', response.contentType, response.length(), 'bytes')
+
+    socket.write(headers)
+    socket.write(new Uint8Array(body))
     socket.end()
 }
--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,16 @@
 import net from 'net'
+import fs from 'fs'
+import { argv } from 'process'
 
 import { Request } from './http/request.js'
 import { respond as _respond } from './http/response.js'
+import { synthasizeArguments } from './args.js'
 
 const server = net.createServer((socket) => socket.on('close', () => socket.end()))
 server.listen(4221, '0.0.0.0')
 console.log('Listening on port 4221')
+
+const options = synthasizeArguments(argv)
 
 server.on('connection', socket => {
     // console.log('Connection established', socket.remoteAddress)
@@ -13,7 +18,7 @@ server.on('connection', socket => {
         // console.log('Received data:', data.toString())
         const request = data.toString(),
             { method, url, path } = Request.title(request), // Parse first request line
-            respond = (code: number | string, message?: string) => _respond(socket, code, message)
+            respond = (code: number | string, payload?) => _respond(socket, code, payload)
 
         console.log(socket.remoteAddress, method, url)
 
@@ -21,6 +26,12 @@ server.on('connection', socket => {
             case undefined: respond(200, 'Welcome!'); break
             case 'echo': respond(200, path[1]); break
             case 'user-agent': respond(200, Request.headers(request)['User-Agent']); break
+            case 'files': 
+                const fileName = path.slice(1), filePath = [options.directory, fileName].join('/')
+                console.log('Looking for file: ' + filePath)
+                if (fs.existsSync(filePath)) respond(200, fs.readFileSync(filePath))
+                else respond(404, fileName + ' file not found')
+                break
             default: respond(404, url + ' not found')
         }
     })
